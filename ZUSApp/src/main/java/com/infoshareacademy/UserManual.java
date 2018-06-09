639083b7872b45f07b2bb0960e9333bb9f67e065
KR-E <kr.eliasz@gmail.com>
2018-06-09 18:50:12 +0200

    added new lines to sout

--- ZUSApp/src/main/java/com/infoshareacademy/UserManual.java
+++ ZUSApp/src/main/java/com/infoshareacademy/UserManual.java
@@ -1,14 +1,45 @@
-package com.infoshareacademy;
+//package com.infoshareacademy;
+//
+// public class UserManual implements AppOperator{
+//
+//     public void execute() {
+//         System.out.println("WITAMY W APLIKACJI ZUS MANUAL\n"+
+//                 "W tym miejscu odnajdziesz informacje niezbędne do prawidłowego korzystania z aplikacji\n\n" +
+//                 "");
+//     }
+//
+//
+// }
 
- public class UserManual implements AppOperator{
+ package com.infoshareacademy;
 
-     public void execute() {
-         System.out.println("WITAMY W APLIKACJI ZUS MANUAL\n"+
-                 "W tym miejscu odnajdziesz informacje niezbędne do prawidłowego korzystania z aplikacji\n\n" +
-                 "");
-     }
+public class UserManual implements AppOperator{
+
+    public void execute() {
+        System.out.println("********************************* WITAMY W APLIKACJI ZUS *********************************\n"+
+                "" +
+                "W tym miejscu odnajdziesz informacje niezbędne do prawidłowego korzystania z aplikacji\n\n" +
+                "OPCJE MENU GŁÓWNEGO:" +
+                "\n1. Wpisz przy użyciu klawiatury \"display-c\" lub \"1\", aby wyświetlić listę dostępnych pojazdów" +
+                "\n\n2. Wpisz przy użyciu klawiatury \"start\" lub \"2\" aby rozpocząć korzystanie z aplikacji" +
+                "\n\n3. Wpisz przy użyciu klawiatury \"manual\" lub \"3\" aby wyświetlić dokumentację apliakcji - " +
+                "\n\n\t\t\t\t\tTO WŁAŚNIE MIEJSCE, W KTÓRYM OBECNIE SIĘ ZNAJDUJESZ" +
+                "\n\nOPCJA ROZPOCZNIJ WYSZUKIWANIE:s" +
+                "\n1. Wpisywanie poszukiwanej marki pojazdu. Aby wybrać tą opcję należy z klawiatury wpisac " +
+                "\ndo konsoli \"1\". Po wpisaniu komendy należy wpisać nazwę poszukiwanej marki samochdodu." +
+                "\n\n2. Przeglądanie marek samochodów po pierwszej litrzeze ich nazwy. Aby wybrać tę opcję" +
+                "\nnależy z klaawiatury wpisać do konscoli \"2\". Po wpisaniu w konsolę dowolnej litery " +
+                "\ni zatwierdzeniu wyświetli się lista dostępnych marek samochodód, któych nazwa zaczyna się " +
+                "\nna wybraną literę " +
+                "\n\n3. Wyswietlanie wszystkich dostępnych marek. Aby skorzystać z tej opcji należy wpisać " +
+                "\nw konsolę programou słowo \"all\"" +
+                "\n****************************************************************************************** ");
 
 
- }
+    }
+}
+
+
+
 
 
